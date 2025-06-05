8e56ec7f013bccc497793b28b6a79aa6b1d33538
varshini012005 <2326ka50@kgcas.com>
2025-06-05 15:31:21 +0530

    Add files via upload

---
+++ Interest.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Interest{
+    public static void main(String[] args) {
+       int p=1000,r=5,t=2;
+         int s=p*r*t/100;
+       System.out.println(s);
+    }
+} 
+        
